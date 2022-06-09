49c951976520b60e33a1c84769fd7ff564058751
曾军杰 <lanfein9@hotmail.com>
2022-06-09 21:41:16 +0800

    saveData

--- src/crowller.ts
+++ src/crowller.ts
@@ -1,41 +1,78 @@
 import superagent from 'superagent'
 import cheerio from 'cheerio'
+import fs from 'fs'
+import path from 'path'
 
 interface ICourse {
     title: string
-    desc: string
     price: string
+}
+interface ICourseData {
+    time: number
+    data: ICourse[]
+}
+interface IfileContent {
+    [key: number]: ICourse[]
 }
 class Crawler {
     private url = `https://yunp.top/app`;
-
+    // 处理课程信息
     public getJsonInfo(html: string) {
         const courseInfos: ICourse[] = []
         const $ = cheerio.load(html);
         const courseRow = $('.course-row');
-        // console.log('courseRow: ', courseRow);
+
         courseRow.map((index, element) => {
             const title = $(element).find('.font-weight-bold').eq(0).text()
-            const desc = $(element).find('.course-meta div').eq(1).text()
             const price = $(element).find('.font-weight-bold').eq(1).text().replace(/[\s]/g, '')
-            courseInfos.push({title,desc,price})
+            courseInfos.push({title,price})
         })
-        const result = {
+        return {
             time: new Date().getTime(),
             data: courseInfos
         }
-        console.log(result);
-
     }
-
+    // 获取课程信息
     public async getRawHtml() {
         const result = await superagent.get(this.url)
-        // console.log('result: ', result.text);
-        // this.rawHtml = result.text;
-        this.getJsonInfo(result.text)
+        return result.text
+    }
+    // 存储课程信息
+    generateJsonFile(courseInfo: ICourseData) {
+        // 文件路径
+        const filePath = path.resolve(__dirname,'../data/course.json')
+        let fileContent: IfileContent = {}
+        // 写入文件,判断文件是否存在
+        if (fs.existsSync(filePath)) {
+            // 存在
+            fileContent = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
+
+            // 读取文件
+            // const fileContent = fs.readFileSync(filePath, 'utf-8')
+            // // 转换成对象
+            // const fileData = JSON.parse(fileContent)
+            // // 合并
+            // const finalData = {
+            //     time: courseInfo.time,
+            //     data: [...fileData.data, ...courseInfo.data]
+            // }
+            // // 写入文件
+            // fs.writeFileSync(filePath, JSON.stringify(finalData))
+        }
+        // 修改内容格式为键值对
+        fileContent[courseInfo.time] = courseInfo.data
+        // 写入文件
+        fs.writeFileSync(filePath, JSON.stringify(fileContent))
+    }
+    // 启动爬虫
+    public async initSpiderProcess() {
+        const html = await this.getRawHtml()
+        const courseResult = this.getJsonInfo(html)
+        // 存储课程信息
+        this.generateJsonFile(courseResult)
     }
     constructor() {
-        this.getRawHtml();
+        this.initSpiderProcess();
     }
 }
 
