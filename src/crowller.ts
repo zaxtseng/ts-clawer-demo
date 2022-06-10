98bf991dd4880ab2bf806c46131ab0ba408aa76d
曾军杰 <lanfein9@hotmail.com>
2022-06-10 10:56:06 +0800

    依据组合设计模式拆分工具

--- src/crowller.ts
+++ src/crowller.ts
@@ -1,74 +1,40 @@
+/**
+ * 纯粹爬取工具类
+ */
 import superagent from 'superagent'
-import cheerio from 'cheerio'
 import fs from 'fs'
 import path from 'path'
+import DellAnalyzer from './dellAnalyzer'
 
-interface ICourse {
-    title: string
-    price: string
-}
-interface ICourseData {
-    time: number
-    data: ICourse[]
-}
-interface IFileContent {
-    [key: number]: ICourse[]
+export interface IAnalyzer {
+    analyze: (html:string, filePath:string) => string
 }
 class Crawler {
-    private url = `https://yunp.top/app`;
     private filePath = path.resolve(__dirname,'../data/course.json')
 
-    // 处理课程信息
-    public getJsonInfo(html: string) {
-        const courseInfos: ICourse[] = []
-        const $ = cheerio.load(html);
-        const courseRow = $('.course-row');
-
-        courseRow.map((index, element) => {
-            const title = $(element).find('.font-weight-bold').eq(0).text()
-            const price = $(element).find('.font-weight-bold').eq(1).text().replace(/[\s]/g, '')
-            courseInfos.push({title,price})
-        })
-        return {
-            time: new Date().getTime(),
-            data: courseInfos
-        }
-    }
+    
     // 获取课程信息
     public async getRawHtml() {
         const result = await superagent.get(this.url)
         return result.text
     }
-    // 存储课程信息
-    generateJsonFile(courseInfo: ICourseData) {
-        // 文件路径
-        const filePath = path.resolve(__dirname,'../data/course.json')
-        let fileContent: IFileContent = {}
-        // 写入文件,判断文件是否存在
-        if (fs.existsSync(filePath)) {
-            // 存在
-            fileContent = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
-        }
-        // 修改内容格式为键值对
-        fileContent[courseInfo.time] = courseInfo.data
-        // 写入文件
-        return fileContent
-    }
+    
     // 写入文件
     public writeFile(filePath: string, data: string) {
-        fs.writeFileSync(filePath, JSON.stringify(data))
+        fs.writeFileSync(filePath, data)
     }
     // 启动爬虫
     public async initSpiderProcess() {
         const html = await this.getRawHtml()
-        const courseResult = this.getJsonInfo(html)
+        const fileContent = this.analyzer.analyze(html, this.filePath)
         // 存储课程信息
-        const fileContent = this.generateJsonFile(courseResult)
-        this.writeFile(this.filePath, JSON.stringify(fileContent))
+        this.writeFile(this.filePath, fileContent)
     }
-    constructor() {
+    constructor(private url: string, private analyzer: IAnalyzer) {
         this.initSpiderProcess();
     }
 }
+const url = `https://yunp.top/app`;
 
-const crawler = new Crawler();
+const analyzer = new DellAnalyzer()
+new Crawler(url, analyzer);
---
+++ src/dellAnalyzer.ts
@@ -0,0 +1,61 @@
+/**
+ * 文件分析工具
+ */
+ import cheerio from 'cheerio'
+ import fs from 'fs'
+import { IAnalyzer } from './crowller'
+
+ interface ICourse {
+    title: string
+    price: string
+}
+ interface ICourseData {
+    time: number
+    data: ICourse[]
+}
+interface IFileContent {
+    [key: number]: ICourse[]
+}
+
+class DellAnalyzer implements IAnalyzer {
+    // 处理课程信息
+    private getJsonInfo(html: string) {
+        const courseInfos: ICourse[] = []
+        const $ = cheerio.load(html);
+        const courseRow = $('.course-row');
+
+        courseRow.map((index, element) => {
+            const title = $(element).find('.font-weight-bold').eq(0).text()
+            const price = $(element).find('.font-weight-bold').eq(1).text().replace(/[\s]/g, '')
+            courseInfos.push({title,price})
+        })
+        // console.log('courseInfos: ', courseInfos);
+        return {
+            time: new Date().getTime(),
+            data: courseInfos
+        }
+    }
+
+    // 存储课程信息
+    generateJsonFile(courseInfo: ICourseData, filePath: string) {
+        // 文件路径
+        // const filePath = path.resolve(__dirname,'../data/course.json')
+        let fileContent: IFileContent = {}
+        // 写入文件,判断文件是否存在
+        if (fs.existsSync(filePath)) {
+            // 存在
+            fileContent = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
+        }
+        // 修改内容格式为键值对
+        fileContent[courseInfo.time] = courseInfo.data
+        // 写入文件
+        return fileContent
+    }
+
+    public analyze(html: string, filePath: string) {
+        const courseResult = this.getJsonInfo(html)
+        const fileContent = this.generateJsonFile(courseResult, filePath)
+        return JSON.stringify(fileContent)
+    }
+}
+export default DellAnalyzer
